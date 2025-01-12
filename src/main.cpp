39dd9cccf5a8046671730fabe448be129af7d10b
dolifero <dimasikolif@gmail.com>
2025-01-12 16:05:14 +0100

    +tests

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dolifero <dolifero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/11 21:30:23 by dolifero          #+#    #+#             */
-/*   Updated: 2025/01/12 15:46:27 by dolifero         ###   ########.fr       */
+/*   Updated: 2025/01/12 16:01:27 by dolifero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,10 @@ int main()
 {
 	SocketPoll poll;
 	Socket s1(8080);
+	Socket s2(8081);
 
 	poll.addFd(s1.getSocketFd());
+	poll.addFd(s2.getSocketFd());
 
 	while(1)
 	{
@@ -25,7 +27,7 @@ int main()
 		{
 			if(poll.canRead(0))
 			{
-				int client_fd = accept(s1.getSocketFd(), NULL, NULL);
+				int client_fd = accept(poll.getFd(0), NULL, NULL);
 				if (client_fd > 0)
 				{
 					const char* response = "HTTP/1.1 200 OK\r\n"
@@ -40,7 +42,10 @@ int main()
 										"<a href='https://www.youtube.com/watch?v=xvFZjo5PgG0'>Something cool, click on me!</a>"
 										"</body></html>\r\n";
 					if(send(client_fd, response, strlen(response), 0))
+					{
+						info_msg("Response sent to FD " + std::to_string(client_fd));
 						close(client_fd);
+					}
 				}
 			}
 		}
