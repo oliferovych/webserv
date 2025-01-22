4919f688b635caf1be851ac332d99825f20ef6ec
oliferovych <dimasikolif@gmail.com>
2025-01-22 01:27:55 +0100

    early stage of config parsing

---
+++ include/config/ServerConfig.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ServerConfig.hpp                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dolifero <dolifero@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/20 15:13:49 by dolifero          #+#    #+#             */
+/*   Updated: 2025/01/22 01:15:51 by dolifero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <unordered_map>
+#include <vector>
+#include <iostream>
+
+class ServerConfig
+{
+	private:
+		std::vector<int> _ports;
+		std::string _root;
+		std::string _index;
+		std::unordered_map<int, std::string> _errorPages;
+		std::vector<std::string> _serverNames;
+		int _valid;
+		void _loadConfig(std::string const &path);
+		bool _parseServer(std::ifstream &file);
+		void _printOut();
+	public:
+		bool isValid() const;
+		ServerConfig(std::string const &path);
+		~ServerConfig();
+};
---
+++ src/config/ServerConfig.cpp
@@ -0,0 +1,131 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ServerConfig.cpp                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dolifero <dolifero@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/20 15:31:35 by dolifero          #+#    #+#             */
+/*   Updated: 2025/01/22 01:26:30 by dolifero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/config/ServerConfig.hpp"
+#include "../../include/global.hpp"
+#include <fstream>
+
+ServerConfig::ServerConfig(std::string const &path)
+{
+	_loadConfig(path);
+}
+
+ServerConfig::~ServerConfig()
+{
+}
+
+bool isKeyWord(std::string const &line, std::string const &keyword)
+{
+	size_t first = std::find_if(line.begin(), line.end(), [](unsigned char ch) {
+		return !std::isspace(ch);
+	}) - line.begin();
+
+	if (first == line.size())
+		return false;
+	size_t last = line.find(' ', first);
+	if (last == std::string::npos)
+		last = line.size();
+	return(line.substr(first, last - first) == keyword);
+}
+
+std::string getSingleVarValue(std::string const &line, std::string const &keyword)
+{
+	size_t pos = line.find(keyword);
+	pos += keyword.length();
+	size_t first = line.find_first_not_of(' ', pos);
+	if(first == std::string::npos)
+		return "";
+	size_t last = line.find(';', first);
+	return line.substr(first, last - first);
+}
+
+bool ServerConfig::_parseServer(std::ifstream &file)
+{
+	std::string line;
+	while(std::getline(file, line))
+	{
+		if(line.empty() || line[0] == '#')
+			continue;
+		if(isLineConsistsOnlyOf(line, "}"))
+		{
+			debug_msg("Server block ended");
+			break;
+		}
+		if(isKeyWord(line, "listen"))
+			_ports.push_back(std::stoi(getSingleVarValue(line, "listen")));
+		// else if(isKeyWord(line, "server_name"))
+		// 	//parse server_name
+		else if(isKeyWord(line, "root"))
+			_root = getSingleVarValue(line, "root");
+		else if(isKeyWord(line, "index"))
+			_index = getSingleVarValue(line, "index");
+		// else if(isKeyWord(line, "error_page"))
+		// {
+		// 	//parse error_page
+		// }
+		else
+		{
+			err_msg("Invalid keyword in server block: " + line);
+			return false;
+		}
+	}
+	return true;
+}
+void ServerConfig::_printOut()
+{
+	std::cout << "Ports: ";
+	for(auto port : _ports)
+		std::cout << port << " ";
+	std::cout << std::endl;
+	std::cout << "Root: " << _root << std::endl;
+	std::cout << "Index: " << _index << std::endl;
+}
+
+void ServerConfig::_loadConfig(std::string const &path)
+{
+	if(path.find(".conf") == std::string::npos)
+	{
+		err_msg("Invalid config file");
+		return ;
+	}
+	std::ifstream file(path.c_str());
+	if (!file.is_open())
+	{
+		err_msg("Error opening config file");
+		return ;
+	}
+	std::string line;
+	while(std::getline(file, line))
+	{
+		if(line.empty() || line[0] == '#')
+			continue;
+		if(isLineConsistsOnlyOf(line, "http {"))
+			continue;
+		if(isLineConsistsOnlyOf(line, "}"))
+			break;
+		if(isLineConsistsOnlyOf(line, "server {"))
+		{
+			if(!_parseServer(file))
+			{
+				_valid = 0;
+				return ;
+			}
+		}
+	}
+	_valid = 1;
+	_printOut();
+}
+
+bool ServerConfig::isValid() const
+{
+	return _valid;
+}
