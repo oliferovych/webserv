dfcb2d285514159a21f0f2ae2a5f84554bfdb7eb
oliferovych <168459324+oliferovych@users.noreply.github.com>
2025-02-21 16:48:29 +0100

    +new display of loaded config

--- include/utils/utils.hpp
+++ include/utils/utils.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tomecker <tomecker@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dolifero <dolifero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/11 21:54:52 by dolifero          #+#    #+#             */
-/*   Updated: 2025/02/17 22:18:14 by tomecker         ###   ########.fr       */
+/*   Updated: 2025/02/21 15:42:04 by dolifero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,7 @@ std::string timestamp();
 void err_msg(const std::string &msg);
 void debug_msg(const std::string &msg);
 void info_msg(const std::string &msg);
+void displayConfigLine(std::string line, int indentation, std::string indentColor);
 
 void ft_trim(std::string &str);
 void ft_tolower(std::string &str);
